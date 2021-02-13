f9c7003ed753b06418255b698ea05207ced0b5ee
grjus <grjus_services+github@pm.me>
2021-02-13 23:20:06 +0100

    Added fatigue calculations endpoint

--- api/main.py
+++ api/main.py
@@ -1,3 +1,21 @@
-from fatigue.fatigue import Fatigue, FatigueModel
+from fastapi import FastAPI
+import uvicorn
+from fatigueWebWrapper.Payload import FatiguePayload
+from fatigueWebWrapper.wrapper import FatigueWebWrapper
 
-Fatigue(-25, 25, 155, FatigueModel.GOODMAN)
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/api/calculations/fatigue")
+async def calculate_fatigue(payload: FatiguePayload):
+    mod_factor = FatigueWebWrapper(payload).get_mod_factor()
+    print(f"MOdification factor {mod_factor}")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
