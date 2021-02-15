abcd5299e00d7778e10603ea584567113faf9845
grjus <grjus_services+github@pm.me>
2021-02-15 21:55:09 +0100

    Added global styles

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from '../../style';
+import TopBar from '../TopBar';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TopBar />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/style.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,900|Roboto");
+  body {
+    font-family:'Notable','Arial',sans-serif;
+    background-color:#E3E2DF;
+    color:black;
+    margin:0;
+    padding:0;
+  }
+`;
+
+export default GlobalStyle;
