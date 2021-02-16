88b143a4ff1037102635bc7fd66a0eecdcadb636
grjus <grjus_services+github@pm.me>
2021-02-16 13:18:57 +0100

    added link to Roboto font

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './style';
 import App from './components/App/App';
 
 ReactDOM.render(
   <HashRouter>
     <React.StrictMode>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </HashRouter>,
   document.getElementById('root'),
--- frontend/src/style.js
+++ frontend/src/style.js
@@ -1,14 +1,47 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { createMuiTheme } from '@material-ui/core';
+
+export const theme = {
+  mainLayoutColor: '#E3E2DF',
+  color: 'rgba(0, 0, 0, 0.8)',
+  topBarColor: '#BAB2B5',
+  logoColor: '#123C69',
+  logoColorHover: '#AC3B61',
+
+};
+
+export const materialUiTheme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Times New Roman',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 const GlobalStyle = createGlobalStyle`
-@import url("https://fonts.googleapis.com/css?family=Montserrat:400,900|Roboto");
   body {
-    font-family:'Notable','Arial',sans-serif;
-    background-color:#E3E2DF;
-    color:black;
+    font-family:'Roboto','Arial',sans-serif;
+    background-color:${(props) => props.theme.mainLayoutColor};
+    color:${(props) => props.theme.color};
     margin:0;
     padding:0;
+    align-items:center;
+    min-width:800px;
+
   }
+`;
+
+export const Container = styled.div`
+background-color:whitesmoke;
+width:80%;
+position:relative;
+top:20px;
+bottom:0;
+left:0;
+right:0;
+margin:auto;
+border-radius:45px;
 `;
 
 export default GlobalStyle;
