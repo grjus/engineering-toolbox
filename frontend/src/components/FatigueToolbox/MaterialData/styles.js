6e1fe163999305b3da4bf3707e911a1146e95b8c
grjus <grjus_services+github@pm.me>
2021-03-13 21:21:19 +0100

    Added flex box to fatigue material container

--- frontend/src/components/FatigueToolbox/MaterialData/styles.js
+++ frontend/src/components/FatigueToolbox/MaterialData/styles.js
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 export const DropdownContainer = styled.div`
 margin-left:10px;
-float:left;
+flex-flow:column wrap; 
+flex-direction:row;
+display:flex;
+justify-content:flex-start
+/* float:left;
 display:grid;
 grid-template-columns: repeat(3, minmax(110px, auto));
-grid-gap:5px;
+grid-gap:5px; */
 `;
