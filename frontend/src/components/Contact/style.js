af72abe32822e7339fbceb93ec80311ca0e87df9
grjus <grjus_services+github@pm.me>
2021-03-15 22:31:24 +0100

    More styling to Contact panel

--- frontend/src/components/Contact/style.js
+++ frontend/src/components/Contact/style.js
@@ -4,5 +4,28 @@ import { theme } from '../../style';
 export const ConfirmMessage = styled.p`
 font-size:16px;
 letter-spacing:0.5px;
+color:${(props) => (props.color ? props.color : theme.logoColor)}
+`;
+
+export const Header = styled.h2`
 color:${theme.logoColor};
+letter-spacing:0.8px;
+word-spacing:2px;
+`;
+
+export const Error = styled.p`
+font-size:14px;
+letter-spacing:0.5px;
+color:${theme.logoColorHover};
+padding:0px;
+margin:0px;
+font-weight:bold;
+`;
+
+export const Description = styled.div`
+color:rgba(0,0,0,0.8);
+letter-spacing:0.2px;
+padding:5px 10px 20px 0px;
+font-size:18px;
+line-height:30px;
 `;
