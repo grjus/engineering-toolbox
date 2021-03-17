c36015fe1c02ea146bcec50375006a7b2fbc21e4
grjus <grjus_services+github@pm.me>
2021-03-17 22:22:03 +0100

    Added first load as state controller

--- frontend/src/components/customHooks/index.js
+++ frontend/src/components/customHooks/index.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import fastApi from '../Api';
 
 const useApiHealth = (interval) => {
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const firstLoad = useRef(true);
 
   useEffect(() => {
     const getApiStatus = async () => {
@@ -17,13 +19,21 @@ const useApiHealth = (interval) => {
         setError(true);
         console.error('Cound not connect to python API');
       }
+      setLoading(false);
     };
+    if (firstLoad.current) {
+      setTimeout(() => {
+        getApiStatus();
+        firstLoad.current = false;
+      }, 3000);
+    }
     let apiInterval;
     if (!error) {
       apiInterval = setInterval(getApiStatus, interval);
     }
+
     return () => clearInterval(apiInterval);
   }, [error, interval]);
-  return error;
+  return [error, loading];
 };
 export default useApiHealth;
