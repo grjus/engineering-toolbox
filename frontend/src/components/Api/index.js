8bc553a4d9f8aedeb966a251b88ab70662ec6540
grjus <grjus_services+github@pm.me>
2021-03-27 21:40:52 +0100

    Addded comment for axios fast api instance

--- frontend/src/components/Api/index.js
+++ frontend/src/components/Api/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 const fastApi = axios.create({
+  /**
+   * Axios instance for API communication
+   * Need to update for production release
+   * Issues with CORS during heroku deployment
+   */
   baseURL: 'http://localhost:8000/',
   timeout: 5000,
   headers: {
