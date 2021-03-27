c21ef5853d488b6644cae18b667d616b951c3090
grjus <grjus_services+github@pm.me>
2021-03-27 19:50:24 +0100

    Fixed typo in about section

--- frontend/src/components/About/SSOT.jsx
+++ frontend/src/components/About/SSOT.jsx
@@ -3,7 +3,7 @@ import Section from './Section';
 
 const SSOT = () => (
   <Section title="Single Source of Truth">
-    You may ask, why do I need a webapp. My spreadsheets works just fine. You are right.
+    You may ask, why do I need a webapp? My spreadsheets works just fine. You are right.
     There is nothing wrong in using a spreadsheet, however there are some downsides.
     The main downside is version control. You don’t have any guarantee
     that each team member is working on the latest version of analysis code.
