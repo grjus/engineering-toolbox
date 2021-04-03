558a4679f2a5be0a134245af8a10e1dcf35289c0
grjus <grjus_services+github@pm.me>
2021-04-03 19:36:32 +0200

    Responsive layout init

--- frontend/src/style.js
+++ frontend/src/style.js
@@ -11,6 +11,9 @@ export const theme = {
 };
 
 const GlobalStyle = createGlobalStyle`
+*{
+  box-sizing:border-box;
+}
   body {
     font-family:'Roboto','Arial',sans-serif;
     background-color:${theme.mainLayoutColor};
@@ -18,7 +21,6 @@ const GlobalStyle = createGlobalStyle`
     margin:0;
     padding:0;
     align-items:center;
-    min-width:800px;
     user-select: none;
     letter-spacing:0.5px;
     color:rgba(0,0,0,0.6);
@@ -29,14 +31,12 @@ const GlobalStyle = createGlobalStyle`
 export const Container = styled.div`
 display:block;
 background-color:${(props) => (props.noBackColor ? theme.mainLayoutColor : 'whitesmoke')};
-width:1050px;
 position:relative;
 top:40px;
 bottom:0;
 left:0;
 right:0;
 margin:auto;
-height: auto;
 transition:height 0.5s ease-in;
 ${({ hidden }) => hidden && `
 height:300px;
