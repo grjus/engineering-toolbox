c7e4e12637201ab84486abd9cd79d5fa56f1dff5
grjus <grjus_services+github@pm.me>
2021-04-05 22:36:29 +0200

    Changed styling for opacity block

--- frontend/src/components/NeuberToolbox/Results/style.js
+++ frontend/src/components/NeuberToolbox/Results/style.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
 margin-top:10px;
+overflow:auto;
 display:flex;
+width:100%;
 justify-content:space-around;
 position:relative;
 height: ${(props) => (props.height ? props.height : '22rem')};
